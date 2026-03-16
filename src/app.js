3c602b727f886dc47d9f15fa342a5476384ad922
gautamkaran <karankumargautam.dev@gmail.com>
2026-03-17 01:24:04 +0530

    fix: update swagger URL to use correct default host and ensure proper parsing

--- src/app.js
+++ src/app.js
@@ -13,10 +13,13 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const file = fs.readFileSync(path.resolve(__dirname, "./swagger.yaml"), "utf8");
+
+
 const swaggerDocument = YAML.parse(
-  file?.replace(
-    "- url: ${{server}}",
-    `- url: ${process.env.VOTING_API_HOST_URL || "http://localhost:8080"}/api/v1`
+  file.replace(/- url: \$\{\{server\}\}/g,
+    `- url: ${
+      process.env.VOTING_API_HOST_URL || "http://localhost:3000"
+    }/api/v1`
   )
 );
 
